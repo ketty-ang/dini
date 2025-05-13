7b5f1bd15e7c5146f545e22767800cccc41a9f4a
kettyang <alavaangelica27@gmail.com>
2025-05-13 13:31:34 +0200

    first commit

---
+++ mi-aplicacion/ejercicios.js
@@ -0,0 +1,4 @@
+// Esperar a que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM cargado completamente"); // Verificar si el script se ejecuta
+})
